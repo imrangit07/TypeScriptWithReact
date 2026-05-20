977f022be75a35ead73e73f5a4b1870201dd17fc
imrangit07 <imranhussain21309@gmail.com>
2026-05-20 11:30:56 +0530

    add first

---
+++ 01_type/src/App.tsx
@@ -0,0 +1,11 @@
+import Greet from "./Greet"
+
+const App = () => {
+  return (
+  <>
+  <Greet name="Imran hussain" />
+  </>
+  )
+}
+
+export default App
---
+++ 01_type/src/Greet.tsx
@@ -0,0 +1,10 @@
+const Greet = (props: { name: string }) => {
+    return (
+        <>
+            <h1>Hello, {props.name}</h1>
+        </>
+    )
+}
+
+export default Greet
+
