ffb7bb1ef4e32ad07021036c112de46e5b478c82
imrangit07 <imranhussain21309@gmail.com>
2026-05-20 12:15:41 +0530

    add all data types

--- 01_type/src/App.tsx
+++ 01_type/src/App.tsx
@@ -1,11 +1,60 @@
-import Greet from "./Greet"
+import Greet from "./Greet";
 
 const App = () => {
   return (
-  <>
-  <Greet name="Imran hussain" />
-  </>
-  )
-}
+    <>
+      {/* STRING */}
+      <Greet
+        title="String Example"
+        name="Imran"
+      />
 
-export default App
+      {/* NUMBER */}
+      <Greet
+        title="Number Example"
+        age={24}
+      />
+
+      {/* BOOLEAN */}
+      <Greet
+        title="Boolean Example"
+        isLoggedin={true}
+      />
+
+      {/* OBJECT */}
+      <Greet
+        title="Object Example"
+        user={{
+          first: "Imran",
+          last: "Hussain",
+        }}
+      />
+
+      {/* ARRAY */}
+      <Greet
+        title="Array Example"
+        skills={["React", "Node", "MongoDB"]}
+      />
+
+      {/* UNION TYPE */}
+      <Greet
+        title="Union Example"
+        id="abc123"
+      />
+
+      {/* OPTIONAL PROP */}
+      <Greet
+        title="Optional Example"
+        message="Welcome"
+      />
+
+      {/* TUPLE */}
+      <Greet
+        title="Tuple Example"
+        tupleData={["Imran", 24]}
+      />
+    </>
+  );
+};
+
+export default App;
--- 01_type/src/Greet.tsx
+++ 01_type/src/Greet.tsx
@@ -1,14 +1,124 @@
 type GreetProps = {
-    name: string
-}
+
+    // STRING
+    title: string;
+
+    // OPTIONAL STRING
+    name?: string;
+
+    // NUMBER
+    age?: number;
+
+    // BOOLEAN
+    isLoggedin?: boolean;
+
+    // OBJECT
+    user?: {
+        first: string;
+        last: string;
+    };
+
+    // ARRAY
+    skills?: string[];
+
+    // UNION TYPE
+    id?: string | number;
+
+    // OPTIONAL PROP
+    message?: string;
+
+    // TUPLE
+    tupleData?: [string, number];
+};
 
 const Greet = (props: GreetProps) => {
+
+    const statusStyle = {
+        background: props.isLoggedin ? "green" : "red",
+        width: "10px",
+        height: "10px",
+        borderRadius: "50%",
+        marginBottom: "10px",
+    };
+
     return (
-        <>
-            <h1>Hello, {props.name}</h1>
-        </>
-    )
-}
+        <div
+            style={{
+                border: "1px solid gray",
+                padding: "15px",
+                marginBottom: "20px",
+            }}
+        >
 
-export default Greet
+            <h2>{props.title}</h2>
 
+            {/* STRING */}
+            {props.name && (
+                <h3>Name: {props.name}</h3>
+            )}
+
+            {/* NUMBER */}
+            {props.age && (
+                <h3>Age: {props.age}</h3>
+            )}
+
+            {/* BOOLEAN */}
+            {props.isLoggedin !== undefined && (
+                <>
+                    <div style={statusStyle}></div>
+
+                    <h3>
+                        {props.isLoggedin
+                            ? "User Logged In"
+                            : "User Offline"}
+                    </h3>
+                </>
+            )}
+
+            {/* OBJECT */}
+            {props.user && (
+                <h3>
+                    Full Name:
+                    {" "}
+                    {props.user.first}
+                    {" "}
+                    {props.user.last}
+                </h3>
+            )}
+
+            {/* ARRAY */}
+            {props.skills && (
+                <>
+                    <h3>Skills:</h3>
+
+                    {props.skills.map((skill, index) => (
+                        <p key={index}>{skill}</p>
+                    ))}
+                </>
+            )}
+
+            {/* UNION */}
+            {props.id && (
+                <h3>ID: {props.id}</h3>
+            )}
+
+            {/* OPTIONAL */}
+            {props.message && (
+                <h3>Message: {props.message}</h3>
+            )}
+
+            {/* TUPLE */}
+            {props.tupleData && (
+                <h3>
+                    Tuple:
+                    {" "}
+                    {props.tupleData[0]}
+                    {" - "}
+                    {props.tupleData[1]}
+                </h3>
+            )}
+        </div>
+    );
+};
+
+export default Greet;
